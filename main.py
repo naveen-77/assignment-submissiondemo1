295489b5a9a86b33d88115d9884176e204838d22
naveen-77 <93330124+naveen-77@users.noreply.github.com>
2021-10-28 23:08:59 +0530

    -

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ altitude=int(input("alitude= "))
 if altitude<1000:
     print("Land the plane")
 elif altitude>1000 and altitude<5000:
-    print("come down")
+    print("come down to 1000")
 else:
     print("go around and try later")
